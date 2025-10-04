7670bd62c4014bc9071a4d98aebba05091925bdf
nazwadi <nazwadi@pm.me>
2025-10-04 18:35:55 -0400

    Updated URL endpoints for API definitions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [1.1.1]
+
+Updated to pull API definitions from new hosted Spire at https://spire.eqemu.dev since original project author, Akkadius, has retired and shut down the original site.
+
 ## [1.1.0]
 
 Implemented sourcing snippets from repository https://github.com/EQEmu/spire-quest-snippets
--- src/app/SpireCompletions.ts
+++ src/app/SpireCompletions.ts
@@ -1,57 +1,61 @@
 import * as vscode from "vscode";
-import {SpireLuaCompletions} from "./SpireLuaCompletions";
-import {SpirePerlCompletions} from "./SpirePerlCompletions";
+import { SpireLuaCompletions } from "./SpireLuaCompletions";
+import { SpirePerlCompletions } from "./SpirePerlCompletions";
 
 // @ts-ignore
 // let api: Object; // spire api response cache
 
 export class SpireCompletions {
-    private c: vscode.ExtensionContext;
-    private lua: SpireLuaCompletions;
-    private perl: SpirePerlCompletions;
+  private c: vscode.ExtensionContext;
+  private lua: SpireLuaCompletions;
+  private perl: SpirePerlCompletions;
 
-    constructor(c: vscode.ExtensionContext) {
-        this.c    = c;
-        this.lua  = new SpireLuaCompletions(c);
-        this.perl = new SpirePerlCompletions(c);
+  constructor(c: vscode.ExtensionContext) {
+    this.c = c;
+    this.lua = new SpireLuaCompletions(c);
+    this.perl = new SpirePerlCompletions(c);
+  }
+
+  async download() {
+    console.time("completions process");
+    const axios = require("axios");
+    let r = await axios.get(
+      "http://spire.eqemu.dev/api/v1/quest-api/definitions"
+    );
+    if (r.status === 200) {
+      this.processDefinitions(r.data.data);
+    }
+    r = await axios.get(
+      "http://spire.eqemu.dev/api/v1/quest-api/vscode-snippets"
+    );
+    if (r.status === 200) {
+      this.processSnippets(r.data.data);
+    }
+    console.timeEnd("completions process");
+  }
+
+  private processDefinitions(api: Object) {
+    this.lua.loadConstants(api);
+    this.lua.loadMethods(api);
+    this.lua.loadEvents(api);
+    this.perl.loadConstants(api);
+    this.perl.loadMethods(api);
+    this.perl.loadEvents(api);
+
+    this.lua.registerCompletionProvider();
+    this.perl.registerCompletionProvider();
+  }
+
+  private processSnippets(data: Object) {
+    let files = {};
+    for (let file in data) {
+      // @ts-ignore
+      files[file] = data[file];
     }
 
-    async download() {
-        console.time("completions process");
-        const axios = require('axios');
-        let r       = await axios.get("http://spire.akkadius.com/api/v1/quest-api/definitions");
-        if (r.status === 200) {
-            this.processDefinitions(r.data.data);
-        }
-        r = await axios.get("http://spire.akkadius.com/api/v1/quest-api/vscode-snippets");
-        if (r.status === 200) {
-            this.processSnippets(r.data.data);
-        }
-        console.timeEnd("completions process");
-    }
-
-    private processDefinitions(api: Object) {
-        this.lua.loadConstants(api);
-        this.lua.loadMethods(api);
-        this.lua.loadEvents(api);
-        this.perl.loadConstants(api);
-        this.perl.loadMethods(api);
-        this.perl.loadEvents(api);
-
-        this.lua.registerCompletionProvider();
-        this.perl.registerCompletionProvider();
-    }
-
-    private processSnippets(data: Object) {
-        let files = {};
-        for (let file in data) {
-            // @ts-ignore
-            files[file] = data[file];
-        }
-
-        // @ts-ignore
-        const manifest = JSON.parse(files["manifest.json"]);
-        this.lua.loadSnippets(files, manifest.files);
-        this.perl.loadSnippets(files, manifest.files);
-    }
+    // @ts-ignore
+    const manifest = JSON.parse(files["manifest.json"]);
+    this.lua.loadSnippets(files, manifest.files);
+    this.perl.loadSnippets(files, manifest.files);
+  }
 }
